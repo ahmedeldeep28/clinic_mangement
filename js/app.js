2cdeda65adfbcefd301402e874ec66077e19f30f
ahmed eldeep <a7medeldeep28@gmail.com>
2022-04-18 21:13:56 +0200

    create auth

--- js/app.js
+++ js/app.js
@@ -13,3 +13,30 @@ var swiper = new Swiper(".mySwiper", {
 
     },
 });
+
+
+let doctor_btn = document.getElementById("doctor");
+let patient_btn = document.getElementById("patient");
+let back_btn = document.querySelectorAll(".modal .modal-body .back");
+let modal_choose = document.getElementById("modal-choose");
+let modal_doctor = document.getElementById("modal-doctor");
+let modal_patient = document.getElementById("modal-patient");
+
+doctor_btn.addEventListener("click",() =>{
+    modal_choose.classList.add("d-none")
+    modal_doctor.classList.toggle("d-none")
+})
+patient_btn.addEventListener("click",() =>{
+    modal_choose.classList.add("d-none")
+    modal_patient.classList.toggle("d-none")
+})
+console.log(back_btn)
+
+back_btn.forEach((el) => {
+    el.addEventListener("click",() =>{
+        modal_choose.classList.remove("d-none")
+        modal_patient.classList.add("d-none")
+        modal_doctor.classList.add("d-none")
+    })
+})
+
