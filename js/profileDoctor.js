2c4412cbffa36b1b6da26a351edc92393f2963fc
ahmed eldeep <a7medeldeep28@gmail.com>
2022-05-08 18:05:38 +0200

    update file

--- js/profileDoctor.js
+++ js/profileDoctor.js
@@ -1,40 +1,40 @@
 //swatch from (about doctor) to (form about)
-let edite_about = document.getElementById("edite_about");
-edite_about.addEventListener("click", () => {
+let edit_about = document.getElementById("edit_about");
+edit_about.addEventListener("click", () => {
     document.getElementById("about").classList.toggle("d-none");
     document.getElementById("form_about").classList.toggle("d-none");
-    edite_about.classList.add("d-none")
+    edit_about.classList.add("d-none")
 });
 
 //swatch from  (form about) to (about doctor) 
-let cancel_edite_about = document.getElementById("cancel_edite_about");
-cancel_edite_about.addEventListener("click", () => {
+let cancel_edit_about = document.getElementById("cancel_edit_about");
+cancel_edit_about.addEventListener("click", () => {
     document.getElementById("about").classList.toggle("d-none");
     document.getElementById("form_about").classList.toggle("d-none");
-    edite_about.classList.toggle("d-none")
+    edit_about.classList.toggle("d-none")
 });
 
 
 //swatch from  (doctor) to (form doctor) 
-let edite_doctor_btn = document.getElementById("edite_doctor");
-edite_doctor_btn.addEventListener("click", () => {
+let edit_doctor_btn = document.getElementById("edit_doctor");
+edit_doctor_btn.addEventListener("click", () => {
     document.getElementById("form_doctor").classList.toggle("d-none");
     document.getElementById("info_doctor").classList.toggle("d-none");
-    edite_doctor_btn.classList.toggle("d-none")
+    edit_doctor_btn.classList.toggle("d-none")
 });
 
 //swatch from  (form doctor)  to (doctor)
-let cancel_edite_doctor = document.getElementById("cancel_edite_doctor");
-cancel_edite_doctor.addEventListener("click", () => {
+let cancel_edit_doctor = document.getElementById("cancel_edit_doctor");
+cancel_edit_doctor.addEventListener("click", () => {
     document.getElementById("form_doctor").classList.toggle("d-none");
     document.getElementById("info_doctor").classList.toggle("d-none");
-    cancel_edite_doctor.classList.toggle("d-none")
+    cancel_edit_doctor.classList.toggle("d-none")
 });
 
 
-let edite_info_btn = document.querySelectorAll(".edite_info");
+let edit_info_btn = document.querySelectorAll(".edit_info");
 let info_value = document.querySelectorAll(".info_value");
-edite_info_btn.forEach((element, index) => {
+edit_info_btn.forEach((element, index) => {
     element.addEventListener("click", () => {
         info_value[index].classList.toggle("d-none")
     })
