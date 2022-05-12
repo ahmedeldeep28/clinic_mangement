4d4bf15beaa2176650790c37826776e453798dac
ahmed eldeep <a7medeldeep28@gmail.com>
2022-05-12 17:51:24 +0200

    upload edite

--- js/profileDoctor.js
+++ js/profileDoctor.js
@@ -67,6 +67,9 @@ function getLocation() {
 function showPosition(position) {
     document.getElementById("lat").setAttribute("value",position.coords.latitude);
     document.getElementById("long").setAttribute("value",position.coords.longitude);
-    get_location_btn.textContent = "It was completed"
+    get_location_btn.textContent = "It was completed";
+    document.getElementById("map").setAttribute("src",`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3428.8846409430103!2d   ${position.coords.longitude}  !3d  ${position.coords.latitude}  !2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sar!2seg!4v1652308954227!5m2!1sar!2seg`)
 }
-get_location_btn.addEventListener("click",() => getLocation())
+get_location_btn.addEventListener("click",() => getLocation());
+
+
