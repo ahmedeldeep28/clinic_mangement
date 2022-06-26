26fc016fd6556ed9db61498150b8c9b9da8f40d4
ahmed eldeep <a7medeldeep28@gmail.com>
2022-06-26 17:35:20 +0200

    edite clinic info

--- js/app.js
+++ js/app.js
@@ -15,3 +15,34 @@ let swiper = new Swiper(".mySwiper", {
 new WOW().init();
 
 
+let input_clinic = document.getElementById("input_clinic"),
+    input_docotr = document.getElementById("input_docotr"),
+    btn_back = document.getElementById("btn_back"),
+    btn_next = document.querySelectorAll(".btn_next");
+
+
+btn_next.forEach((el) => {
+    el.addEventListener("click", () => {
+        input_clinic.classList.toggle("d-none");
+        input_docotr.classList.toggle("d-none");
+    })
+})
+btn_back.addEventListener("click", () => {
+    input_clinic.classList.toggle("d-none");
+    input_docotr.classList.toggle("d-none");
+})
+
+
+const image_input = document.querySelectorAll(".image-input");
+
+image_input.forEach((el) => {
+    el.addEventListener("change", function () {
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            const uploaded_image = reader.result;
+            el.parentElement.style.backgroundImage = `url(${uploaded_image})`;
+            el.nextElementSibling.style.display = "none";
+        });
+        reader.readAsDataURL(this.files[0]);
+    })
+});
